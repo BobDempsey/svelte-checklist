c226b31c958048706155223cd0dd47ae283cb9b1
Bob Dempsey <bobdempsey83@gmail.com>
2023-02-11 11:23:18 -0500

    added page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1 class="text-3xl font-bold  underline">Hello world!</h1>
+<div
+	class="selection:color-secondary selection:color-primary min-h-screen bg-base-200 p-0 md:px-8 md:py-24"
+>
+	<main
+		class="mx-auto min-h-screen max-w-none rounded-none bg-base-100 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-base-100"
+	>
+		<h1 class="text-2xl font-bold">Dashboard</h1>
+
+		<div class="mt-10" />
+	</main>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
-			}
-		}
+		extend: {}
 	},
 	plugins: [require('daisyui')]
 };
